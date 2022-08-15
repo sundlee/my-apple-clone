ce48e3091ab93b8b709d331d06a7a6c7cc28d5f1
sundlee <sundlee@gmail.com>
2022-08-15 11:54:19 +0900

    현재 활성시킬 씬 설정

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 (() => {
 
+  let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치 (yOffset) 보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값이 합
+  let currentScene = 0; // 현재 활성화된 (눈 앞에 보고 있는) 씬 (scroll-scene)
+
   const sceneInfo = [
     {
       // 0
@@ -48,13 +52,29 @@
     }
   }
 
-  let yOffset = 0;
+  
   function scrolloop() {
-    yOffset = window.pageYOffset;
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      
+      currentScene--;
+    }
+
+    console.log(currentScene);
   }
 
   window.addEventListener('resize', setLayout);
   window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset;
     scrolloop();
   });
 
