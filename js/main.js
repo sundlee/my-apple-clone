1797d3680387b2c4073af5d45420e62b2a9e3edc
sundlee <sundlee@gmail.com>
2022-08-16 19:32:32 +0900

    스크롤 애니매이션 구현 4

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
   let yOffset = 0; // window.pageYOffset 대신 쓸 변수
   let prevScrollHeight = 0; // 현재 스크롤 위치 (yOffset) 보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값이 합
   let currentScene = 0; // 현재 활성화된 (눈 앞에 보고 있는) 씬 (scroll-scene)
+  let enterNewScene = false; // 새로운 씬이 시작되는 순간 true
 
   const sceneInfo = [
     {
@@ -104,23 +105,28 @@
   }
   
   function scrolloop() {
+    enterNewScene = false;
     prevScrollHeight = 0;
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
 
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     if (yOffset < prevScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) return;
 
       currentScene--;
+
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
+    if (enterNewScene) return;
     playAnimation();
   }
 
