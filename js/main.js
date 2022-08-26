31adaa0f1be3a57f853eca740f65c514b5c372b0
sundlee <sundlee@gmail.com>
2022-08-26 13:46:33 +0900

    동적 위치와 크기 계산을 이용한 스크롤 인터랙션 구현 - 스크롤에 반응하는 메뉴

--- js/main.js
+++ js/main.js
@@ -146,6 +146,14 @@
 	}
   setCanvasImages();
 
+	function checkMenu() {
+		if (yOffset > 44) {
+			document.body.classList.add('local-nav-sticky');
+		} else {
+			document.body.classList.remove('local-nav-sticky');
+		}
+	}
+
 	function setLayout() {
 		// 각 스크롤 섹션의 높이 세팅
 		for (let i = 0; i < sceneInfo.length; i++) {
@@ -484,10 +492,11 @@
   }
 
 
-        window.addEventListener('scroll', () => {
-            yOffset = window.pageYOffset;
-            scrollLoop();
-  		});
+	window.addEventListener('scroll', () => {
+		yOffset = window.pageYOffset;
+		scrollLoop();
+		checkMenu();
+	});
   window.addEventListener('load', () => {
     setLayout();
     sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
