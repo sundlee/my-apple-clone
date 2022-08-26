49618544a6704ce70bbe31d23939ca866da83e0d
sundlee <sundlee@gmail.com>
2022-08-26 12:23:24 +0900

    동적 위치와 크기 계산을 이용한 스크롤 인터랙션 구현 - 이미지 블렌딩

--- js/main.js
+++ js/main.js
@@ -113,6 +113,7 @@
 			values: {
 				rect1X: [ 0, 0, { start: 0, end: 0 } ],
 				rect2X: [ 0, 0, { start: 0, end: 0 } ],
+				blendHeight: [ 0, 0, { start: 0, end: 0 } ],
         rectStartY: 0,
       },
     },
@@ -405,6 +406,18 @@
 					objs.canvas.classList.remove('sticky');
 				} else {
 					step = 2;
+
+					values.blendHeight[0] = 0;
+					values.blendHeight[1] = objs.canvas.height;
+					values.blendHeight[2].start = values.rect1X[2].end;
+					values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+					const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+					objs.context.drawImage(objs.images[1],
+						0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+						0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+					);
+
 					objs.canvas.classList.add('sticky');
 					objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
 				}
